4f516a2c61f3f13327ea8e2a36d9211835395fd4
amk1971 <abdul.m.khan@gmail.com>
2019-11-20 14:31:25 +0500

    Update robobit.ts

--- robobit.ts
+++ robobit.ts
@@ -10,13 +10,7 @@ enum Led {
  */
 //% color=#104242 icon="\uf1b9" block="robo:bit"
 namespace robobit {
-	/**
-	 * Sets the color and brightness of LED.
-     * @param LED #
-     * @param Red Value, eg: 8
-     * @param Green Value, eg: 8
-     * @param Blue Value, eg: 8
-	 */
+
     export function setLEDColor(LED: Led, Red: number, Green: number, Blue: number): void {
 
         pins.spiWrite(LED + Red)
