6041e623a6210895297a992f5287172e853593b7
amk1971 <abdul.m.khan@gmail.com>
2019-11-20 14:21:56 +0500

    Updates.

--- robobit.ts
+++ robobit.ts
@@ -13,18 +13,14 @@ namespace robobit {
 	/**
 	 * Sets the color and brightness of LED.
      * @param LED #
-     * @param Red Value, eg: 8
-     * @param Green Value, eg: 8
-     * @param Blue Value, eg: 8
+     * @param cRed Value, eg: 8
+     * @param cGreen Value, eg: 8
+     * @param cBlue Value, eg: 8
 	 */
-    //% Red.min=0 Red.max=16
-    //% Green.min=0 Green.max=16
-    //% Blue.min=0 Blue.max=16
-    //% weight=80
-    export function setLEDColor(LED: Led, Red: number, Green: number, Blue: number): void {
+    export function setLEDColor(LED: Led, cRed: number, cGreen: number, cBlue: number): void {
 
-        pins.spiWrite(LED + Red)
-        pins.spiWrite(Green * 16 + Blue)
+        pins.spiWrite(LED + cRed)
+        pins.spiWrite(cGreen * 16 + cBlue)
     }
 
 }
