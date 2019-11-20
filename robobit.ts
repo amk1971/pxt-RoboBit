f375d2b4b012d3e6cb8527a880bc919845640c23
amk1971 <abdul.m.khan@gmail.com>
2019-11-20 14:42:17 +0500

    Update robobit.ts

--- robobit.ts
+++ robobit.ts
@@ -6,15 +6,27 @@ enum Led {
 }
 
 /**
- * Functions to operate the robo:bit
+ * Functions to operate the roboo:bit
  */
 //% color=#104242 icon="\uf1b9" block="robo:bit"
 namespace robobit {
+	/**
+	 * Sets the color and brightness of LED.
+     * @param LED #
+     * @param brightness percent of maximum brightness, eg: 50
+	 */
+    //% blockId="robobit_setLED" block="light %LED_number %path|at %brightness|%"
+    //% brightness.min=0 brightness.max=100
+    //% weight=80
+    export function setLEDColor(LED: Led, brightness: number): void {
+        let pwr = 0
+        brightness = Math.abs(brightness)
+        if (brightness > 100) {
+            brightness = 100
+        }
 
-    export function setLEDColor(LED: Led, Red: number): void {
-
-        pins.spiWrite(LED + Red)
-        pins.spiWrite(10)
+         pins.spiWrite(0x20)
+		 pins.spiWrite(brightness)
     }
 
 }
