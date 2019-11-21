4f891e50cb0a2ac2ac9e5d2644779c739035f80d
amk1971 <abdul.m.khan@gmail.com>
2019-11-21 10:03:58 +0500

    Motor support is added

--- robobit.ts
+++ robobit.ts
@@ -5,19 +5,37 @@ enum Led {
     Right = 64
 }
 
+enum Motor {
+    //% block="left"
+    Left = 17,
+    //% block="right"
+    Right = 18
+}
 /**
  * Functions to operate the robo:bit
  */
 //% color=#104242 icon="\uf1b9" block="robo:bit"
 namespace robobit {
 	/**
+	 * Sets the Speed of LED.
+     * @param Motor #
+     * @param Speed Speed of Motor, eg: 50
+	 */
+    //% blockId="robobit_setMotorSpeed" block="MOTOR %Motor Speed %S"
+    //% R.min=0 R.max=100
+    //% weight=80
+    export function setMotorSpeed(MOTOR: Motor, Speed: number): void {
+        pins.spiWrite(MOTOR)
+        pins.spiWrite(Speed)
+    }
+    /**
 	 * Sets the color and brightness of LED.
      * @param LED #
      * @param R brightness, eg: 8
      * @param G brightness, eg: 8
      * @param B brightness, eg: 8
 	 */
-    //% blockId="robobit_setLED" block="LED %LED Color_is_as Red %R Green %G Blue %B"
+    //% blockId="robobit_setLED" block="LED %LED Red %R Green %G Blue %B"
     //% R.min=0 R.max=15
     //% G.min=0 G.max=15
     //% B.min=0 B.max=15
